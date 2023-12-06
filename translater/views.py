5f339efcd8df7fd1b9691fc5f5c97bf86758ffe8
aruuuuuuke <askatovaaruuke657@gmail.com>
2023-12-06 10:20:24 +0300

    deploy

--- translater/views.py
+++ translater/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Group
 from .serializers import GroupSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
-class GroupAPIView(generics.ListAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+class GroupAPIView(APIView):
+    def post(self, request):
+        return Response({'name':'food'})
+
+
+# class GroupAPIView(generics.ListAPIView):
+#     queryset = Group.objects.all()
+#     serializer_class = GroupSerializer
